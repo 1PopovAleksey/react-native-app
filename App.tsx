68226e7566ee0f9ac856d6387957242d1d2641c4
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-08-05 11:31:02 +0300

    Refactoring App.tsx

--- App.tsx
+++ App.tsx
@@ -1,27 +1,15 @@
-import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View} from 'react-native';
+import {StatusBar, View} from 'react-native';
+import {Header} from "./components/Header";
+import {HomeScreen} from "./screens/Home";
+import {FullPostScreen} from "./screens/FullPosts";
 
 export default function App() {
+
     return (
-        <View style={styles.container}>
-            <Text>Hello, world!</Text>
-            <Text style={styles.par}>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus deserunt earum eveniet nulla
-                numquam odio repellat vero voluptatem.
-            </Text>
-            <StatusBar style="auto"/>
+        <View>
+            <Header/>
+            <HomeScreen/>
+            <StatusBar/>
         </View>
     );
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    par: {
-        width: '90%',
-    }
-});
+};
